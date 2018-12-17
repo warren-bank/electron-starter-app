2a37c889af7c81341d4e9232e2246e437dff7457
Warren R Bank <warren.r.bank@gmail.com>
2018-12-17 13:58:07 -0800

    lint webpack configs

--- webpack.config.main.js
+++ webpack.config.main.js
@@ -3,47 +3,47 @@ const nod_mod = path.resolve('./node_modules') + path.sep
 const webpack = require(nod_mod + 'webpack')
 
 module.exports = {
-   entry: './src/main/index.js',
-    output: {
-        path:     path.resolve(__dirname, 'app', 'bundles'),
-        filename: 'main.js'
-    },
-    resolve: {
-        modules: [
-            nod_mod.substring(0, nod_mod.length-1),
-            path.resolve('./src/node_modules'),
-            path.resolve('./src/main'),
-            path.resolve('./src')
-        ]
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(?:js|jsx)$/i,
-                exclude: /\/node_modules\/(?!@warren-bank\/)/,
-                loader: nod_mod + 'babel-loader',
-                query: {
-                    presets: [nod_mod + 'babel-preset-' + 'env', nod_mod + 'babel-preset-' + 'stage-0']
-                }
-            }
-        ]
-    },
-    plugins: [
-        new webpack.DefinePlugin({
-            "process.env": {
-                NODE_ENV: JSON.stringify('production')
-            }
-        }),
-        new webpack.optimize.UglifyJsPlugin({
-            sourceMap: false,
-            warnings: false,
-            mangle: true,
-            cache: false
-        })
-    ],
-    node: {
-        __filename: true,
-        __dirname: true,
-    },
-    target: 'electron-main'
+  entry: './src/main/index.js',
+  output: {
+    path:     path.resolve(__dirname, 'app', 'bundles'),
+    filename: 'main.js'
+  },
+  resolve: {
+    modules: [
+      nod_mod.substring(0, nod_mod.length-1),
+      path.resolve('./src/node_modules'),
+      path.resolve('./src/main'),
+      path.resolve('./src')
+    ]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(?:js|jsx)$/i,
+        exclude: /\/node_modules\/(?!@warren-bank\/)/,
+        loader: nod_mod + 'babel-loader',
+        query: {
+          presets: [nod_mod + 'babel-preset-' + 'env', nod_mod + 'babel-preset-' + 'stage-0']
+        }
+      }
+    ]
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      "process.env": {
+        NODE_ENV: JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      sourceMap: false,
+      warnings: false,
+      mangle: true,
+      cache: false
+    })
+  ],
+  node: {
+    __filename: true,
+    __dirname: true,
+  },
+  target: 'electron-main'
 }
--- webpack.config.renderer.js
+++ webpack.config.renderer.js
@@ -3,65 +3,65 @@ const nod_mod = path.resolve('./node_modules') + path.sep
 const webpack = require(nod_mod + 'webpack')
 
 module.exports = {
-    entry: {
-      index:   './src/renderer/index.js',
-      webview: './src/renderer/webview.js'
-    },
-    output: {
-        path:              path.resolve(__dirname, 'app', 'bundles'),
-        filename:          '[name].js',
-        sourceMapFilename: '[name].map'
-    },
-    devtool: '#source-map',
-    resolve: {
-        modules: [
-            nod_mod.substring(0, nod_mod.length-1),
-            path.resolve('./src/node_modules'),
-            path.resolve('./src/renderer'),
-            path.resolve('./src')
-        ]
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(?:js|jsx)$/i,
-                exclude: /\/node_modules\/(?!@warren-bank\/)/,
-                loader: nod_mod + 'babel-loader',
-                query: {
-                    presets: [nod_mod + 'babel-preset-' + 'env', nod_mod + 'babel-preset-' + 'stage-0', nod_mod + 'babel-preset-' + 'react']
-                }
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    nod_mod + 'style-loader',
-                    nod_mod + 'css-loader',
-                    {
-                        loader: nod_mod + 'postcss-loader',
-                        options: {
-                            plugins: () => [require(nod_mod + 'autoprefixer')]
-                        }
-                    }
-                ]
+  entry: {
+    index:   './src/renderer/index.js',
+    webview: './src/renderer/webview.js'
+  },
+  output: {
+    path:              path.resolve(__dirname, 'app', 'bundles'),
+    filename:          '[name].js',
+    sourceMapFilename: '[name].map'
+  },
+  devtool: '#source-map',
+  resolve: {
+    modules: [
+      nod_mod.substring(0, nod_mod.length-1),
+      path.resolve('./src/node_modules'),
+      path.resolve('./src/renderer'),
+      path.resolve('./src')
+    ]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(?:js|jsx)$/i,
+        exclude: /\/node_modules\/(?!@warren-bank\/)/,
+        loader: nod_mod + 'babel-loader',
+        query: {
+          presets: [nod_mod + 'babel-preset-' + 'env', nod_mod + 'babel-preset-' + 'stage-0', nod_mod + 'babel-preset-' + 'react']
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          nod_mod + 'style-loader',
+          nod_mod + 'css-loader',
+          {
+            loader: nod_mod + 'postcss-loader',
+            options: {
+              plugins: () => [require(nod_mod + 'autoprefixer')]
             }
+          }
         ]
-    },
-    plugins: [
-        new webpack.DefinePlugin({
-            "process.env": {
-                NODE_ENV: JSON.stringify('production')
-            }
-        }),
-        new webpack.optimize.UglifyJsPlugin({
-            sourceMap: true,
-            warnings: false,
-            mangle: true,
-            cache: false
-        })
-    ],
-    node: {
-        __filename: true,
-        __dirname: true,
-    },
-    target: 'electron-renderer'
+      }
+    ]
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      "process.env": {
+        NODE_ENV: JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      sourceMap: true,
+      warnings: false,
+      mangle: true,
+      cache: false
+    })
+  ],
+  node: {
+    __filename: true,
+    __dirname: true,
+  },
+  target: 'electron-renderer'
 }
