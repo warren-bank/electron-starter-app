356ccae3a7baff43af4587e169093d1eebbba9b5
Warren R Bank <warren.r.bank@gmail.com>
2018-12-17 18:40:23 -0800

    webpack configs: update "DefinePlugin" configs
    issue:
    ======
    * in the 'portable' Windows target,
      the 'PORTABLE_EXECUTABLE_DIR' environment variable was not defined
      * see:
        https://github.com/electron-userland/electron-builder/blob/56ddb8ee4c1d99582ee7864443c6b18e749df6f5/packages/app-builder-lib/templates/nsis/portable.nsi#L36
    
    the cause:
    ==========
    * previously, the webpack "DefinePlugin" was configured to replace:
      * the entire 'process.env' Object
    * now, the configs are updated to:
      * preserve environment variables
      * selectively modify individual environment variable key(s)

--- webpack.config.main.js
+++ webpack.config.main.js
@@ -33,9 +33,7 @@ module.exports = {
   mode: 'production',
   plugins: [
     new webpack.DefinePlugin({
-      "process.env": {
-        NODE_ENV: JSON.stringify('production')
-      }
+      "process.env.NODE_ENV": JSON.stringify('production')
     }),
     new TerserPlugin({
       sourceMap: false,
--- webpack.config.renderer.js
+++ webpack.config.renderer.js
@@ -51,9 +51,7 @@ module.exports = {
   mode: 'production',
   plugins: [
     new webpack.DefinePlugin({
-      "process.env": {
-        NODE_ENV: JSON.stringify('production')
-      }
+      "process.env.NODE_ENV": JSON.stringify('production')
     }),
     new TerserPlugin({
       sourceMap: true,
