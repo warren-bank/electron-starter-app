b46c2452f7a0abd81051bf38d6a4a449d16dd040
Warren R Bank <warren.r.bank@gmail.com>
2018-12-26 13:15:37 -0800

    bug fix

--- src/common/utils.js
+++ src/common/utils.js
@@ -2,8 +2,8 @@ const path = require('path')
 
 const utils = {}
 
-utils.resolve_relative_filepath = (app, filepath) => path.resolve(app.getAppPath(), filepath)
+utils.resolve_relative_filepath = (app, ...filepath) => path.resolve(app.getAppPath(), ...filepath)
 
-utils.convert_relative_filepath_to_URL = (app, filepath) => 'file://' + utils.resolve_relative_filepath(app, filepath).replace(/[\\]/g, '/')
+utils.convert_relative_filepath_to_URL = (app, ...filepath) => 'file://' + utils.resolve_relative_filepath(app, ...filepath).replace(/[\\]/g, '/')
 
 module.exports = utils
